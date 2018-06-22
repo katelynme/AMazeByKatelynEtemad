8bb8a1f7c8a1d917c66c683bace00ca674f08bba
Katelyn Etemad <kmetemad@email.wm.edu>
2018-06-22 15:46:45 -0400

    initial commit

---
+++ app/src/main/java/katelynetemad/cs301/cs/wm/edu/amazebykatelynetemad/AMazeActivity.java
@@ -0,0 +1,13 @@
+package katelynetemad.cs301.cs.wm.edu.amazebykatelynetemad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AMazeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amaze);
+    }
+}
