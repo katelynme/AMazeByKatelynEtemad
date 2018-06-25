ed3447edfd0fc6feff8c64f981bf8b0c63cd03ed
Katelyn Etemad <kmetemad@email.wm.edu>
2018-06-24 21:22:39 -0400

    added GeneratingActivity

--- app/src/main/java/katelynetemad/cs301/cs/wm/edu/amazebykatelynetemad/gui/AMazeActivity.java
+++ app/src/main/java/katelynetemad/cs301/cs/wm/edu/amazebykatelynetemad/gui/AMazeActivity.java
@@ -1,15 +1,39 @@
 package katelynetemad.cs301.cs.wm.edu.amazebykatelynetemad.gui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import katelynetemad.cs301.cs.wm.edu.amazebykatelynetemad.R;
 
 public class AMazeActivity extends AppCompatActivity {
+    private static final String TAG = "AMazeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amaze);
+    }
+
+    /**
+     * Method to switch to the generating activity with the proper specifications
+     * when the explore button is clicked
+     */
+    public void onExploreClick(View view){
+        Intent i = new Intent(this, GeneratingActivity.class);
+        Log.v(TAG, "Starting the generating activity through explore");
+        startActivity(i);
+    }
+
+    /**
+     * Method to switch to the generating activity with the proper specifications
+     * when the revisit button is clicked
+     */
+    public void onRevisitClick(View view){
+        Intent i = new Intent(this, GeneratingActivity.class);
+        Log.v(TAG, "Starting the generating activity through revisit");
+        startActivity(i);
     }
 }
