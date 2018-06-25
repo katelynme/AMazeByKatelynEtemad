bdcd365b4bf3583f41854422cb569c074a8f5d61
Katelyn Etemad <kmetemad@email.wm.edu>
2018-06-24 21:37:20 -0400

    updated GeneratingActivity

---
+++ app/src/main/java/katelynetemad/cs301/cs/wm/edu/amazebykatelynetemad/gui/FinishActivity.java
@@ -0,0 +1,15 @@
+package katelynetemad.cs301.cs.wm.edu.amazebykatelynetemad.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import katelynetemad.cs301.cs.wm.edu.amazebykatelynetemad.R;
+
+public class FinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+    }
+}
--- app/src/main/java/katelynetemad/cs301/cs/wm/edu/amazebykatelynetemad/gui/GeneratingActivity.java
+++ app/src/main/java/katelynetemad/cs301/cs/wm/edu/amazebykatelynetemad/gui/GeneratingActivity.java
@@ -1,15 +1,34 @@
 package katelynetemad.cs301.cs.wm.edu.amazebykatelynetemad.gui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
 
 import katelynetemad.cs301.cs.wm.edu.amazebykatelynetemad.R;
 
 public class GeneratingActivity extends AppCompatActivity {
+    private static final String TAG = "GeneratingActivity";
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generating);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+        progressBar.setProgress(50);
+        Intent i = new Intent(this, PlayAnimationActivity.class);
+        Log.v(TAG, "Starting the PlayAnimationActivity");
+        startActivity(i);
+    }
+
+    /**
+     * Code for when the back button is pressed, the app should return to the
+     * main screen (AMazeActivity).
+     */
+    public void onBackPressed(){
+        Log.v(TAG, "Starting the generating activity through explore");
     }
 }
